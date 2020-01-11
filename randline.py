f021879d85e32119609bf2278134b035f046d230
Stefan Lachmann <stefanlachmann@hotmail.com>
2020-01-11 15:51:22 +0100

    added script to source, fixed missing "s" in new greeting and format randline

--- randline.py
+++ randline.py
@@ -4,6 +4,7 @@ import random
 import sys
 import datetime
 
+
 def timeOfDay(time):
     if time.hour <= 2 or time.hour >= 18:
         return 'evening'
@@ -12,14 +13,17 @@ def timeOfDay(time):
     else:
         return 'afternoon'
 
+
 def todayOrTonight(time):
     if time.hour <= 4 or time.hour >= 20:
         return 'tonight'
     else:
         return 'today'
 
+
 def dayOfWeek(time):
     return time.strftime('%A')
+
 
 def main():
     lines = open(sys.argv[1]).readlines()
@@ -32,6 +36,6 @@ def main():
         'name': name
     })
 
+
 if __name__ == "__main__":
     main()
-
